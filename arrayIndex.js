19d5bbc8649f4445a5f892d089fc7eceb9318a82
MstIsratJahanAnika <mstisratjahananika@gmail.com>
2026-01-27 00:35:15 +0600

    array indexing

---
+++ arrayIndex.js
@@ -0,0 +1,14 @@
+const numbers = [10, 34, 688, 342, 12, 90, 57];
+console.log(numbers);
+
+console.log(numbers[0]); //output 0th index
+console.log(numbers[3]); // output 3rd index 
+
+const forth = numbers[3];
+console.log(forth);
+
+let abc = 45;
+abc = 99; // value update
+
+numbers[2] = abc; // value update at the index 
+console.log(numbers);
---
+++ length.js
@@ -0,0 +1,4 @@
+const numbers = [10, 20, 11, 33, 47, 89, 65, 40, 32, 15, 16, 60, 47, 89, 65, 40, 32, 15];
+
+//find array length
+console.log(numbers.length);
