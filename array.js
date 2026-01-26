cb7e9af16e3223b3b7a49cea1cb492725e447904
MstIsratJahanAnika <mstisratjahananika@gmail.com>
2026-01-27 00:00:55 +0600

    array declaration in JS

---
+++ array.js
@@ -0,0 +1,9 @@
+const num1 = 12;
+const num2 = 22; 
+const num3 = 121; 
+const num4 = 441;
+
+const numbers = [12, 22, 121, 441]; // an array
+const friends = ['abul', 'babul', 'kabul', 'dabul', 'bulbul']; //string array
+const mixedArray = [24, 'hashmot', 56.45, 'morjina'];
+const isPassed = [true, false, false, false, true, true,true];
