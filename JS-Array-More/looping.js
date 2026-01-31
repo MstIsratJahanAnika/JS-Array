d8e89a4a96f9aba9d139172616015bc2c9febaa6
MstIsratJahanAnika <mstisratjahananika@gmail.com>
2026-02-01 01:34:56 +0600

    looping in array

--- JS-Array-More/looping.js
+++ JS-Array-More/looping.js
@@ -14,3 +14,45 @@ for(const friend of friends)
 {
     console.log(friend);
 }
+
+for(let i=0; i<10; i++)
+{
+    // console.log(i);
+    // console.log(friends[i]);
+}
+
+//for solving the excess array indexing - loop upto length 
+
+//for loop example 
+//loop - 1
+for(let i=0; i< friends.length; i++)
+{
+    console.log(i);
+    console.log(friends[i]);
+}
+
+//loop - 2 
+const numbers = [23, 490, 7459, 109, 7597, 6857, 231, 14];
+for(let n=0; n< numbers.length; n++)
+{
+    console.log(numbers[n]);
+}
+
+
+
+//while loop examples 
+//loop - 1
+let i = 0;
+while(i< friends.length)
+{
+    console.log(friends[i]);
+    i++;
+}
+
+//loop - 2 
+let n = 0; 
+while(n < numbers.length)
+{
+    console.log(numbers[n]);
+    n++;
+}
