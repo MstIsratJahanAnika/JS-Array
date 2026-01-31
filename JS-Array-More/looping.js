786e2aa0313309c25fccf41d24db53a2e5aa05ad
MstIsratJahanAnika <mstisratjahananika@gmail.com>
2026-01-31 20:44:12 +0600

    looping initialized

---
+++ JS-Array-More/looping.js
@@ -0,0 +1,16 @@
+/**
+ * Looping Techniques
+ * 1. for loop 
+ * 2. while loop
+ * 3. do-while loop  --> ignore
+ * 4. for-of --> use for array loop
+ * 5. for-in --> use for object loop
+ * 
+ */
+
+const friends = ['Elon', 'Bill', 'Mark', 'Waren',];
+
+for(const friend of friends)
+{
+    console.log(friend);
+}
